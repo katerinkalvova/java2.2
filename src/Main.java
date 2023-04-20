046597c830f0456fcb5e00d4e28778b91c7e1176
Katerina Lvova <katerinkalvova@seznam.cz>
2023-04-20 15:05:55 +0200

    -a

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        int money = 300;
+        int amount = 100;
+        int initialMoney = 100;
+
+        int bonus;
+        if (money >= 1000) {
+            bonus = money / amount;
+        }else{ bonus = 0;
+
+        }
+
+
+        int totalMoney = initialMoney + money + bonus;
+
+
+        System.out.println ("сумма на счету "+ totalMoney );
+        System.out.println("Бонус " + bonus);
+
+    }
+
+}
